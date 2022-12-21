3d9bae24d6f63e8b0a3e23d2cb868440a10f60c6
Anton Oshurek <oshurekweb@gmail.com>
2022-12-21 14:09:18 +0100

    add log api

--- cli-weather/weather.js
+++ cli-weather/weather.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log-service.js";
 
 const initCLI = () => {
   console.log('init CLI');
-
   const args = getArgs(process.argv);
-  console.log(args);
 
   if(args.h) {
     //вывод help
+    printHelp();
   }
   if(args.s) {
     //сохранить город
