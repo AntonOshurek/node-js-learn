1c910f4623473c291bab19909f9cfb0442799c4d
Anton Oshurek <oshurekweb@gmail.com>
2022-12-22 15:51:24 +0100

    midleweare

--- dashboard-api/index.js
+++ dashboard-api/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
 
+import { userRouter } from './users/users.js';
+
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('время ' + Date.now());
+  next();
+});
+
 app.get('/hello', (req, res) => {
-  res.send('Hello server :)');
+  // res.download('/test.pdf', 'fileName.pdf');
+  throw new Error('ERROR!-------')
+});
+
+app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message);
 });
 
 app.listen(port, () => {
