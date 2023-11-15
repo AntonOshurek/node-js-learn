971ced4c5866bbb962be120c54beee5f35ff4547
Anton Ashurek <antonashurek@192.168.1.5>
2023-11-15 20:03:02 +0100

    create auth dto

--- nest-server/src/auth/auth.controller.ts
+++ nest-server/src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode } from '@nestjs/common';
 import { Post } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
 	@Post('register')
-	async register() {}
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async register(@Body() dto: AuthDto) {}
 
+	@HttpCode(200)
 	@Post('login')
 	async login() {}
 }
