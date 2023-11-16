460ab13d8101fe8f4344e2e65ef773bcf0d019d7
Anton Ashurek <antonashurek@192.168.1.5>
2023-11-16 21:39:55 +0100

    add mongoose

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -6,9 +6,11 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
 	imports: [
+		MongooseModule.forRoot('mongodb://localhost/test'),
 		ConfigModule.forRoot(),
 		AuthModule,
 		TopPageModule,
