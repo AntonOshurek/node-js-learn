34442ed23b95f336da92d24fabf16007b910e995
Anton <oshurekdev@proton.me>
2024-08-04 16:28:48 +0200

    add helmet

--- nest-user-app/src/main.ts
+++ nest-user-app/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module.js';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api');
+	app.use(helmet());
 	app.use(cookieParser());
 
 	await app.listen(3000);
