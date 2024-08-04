e11a5f73dc7f416b1865a123133d131e9aa2aa46
Anton <oshurekdev@proton.me>
2024-08-04 16:51:05 +0200

    loging and error handling styling

--- nest-user-app/src/main.ts
+++ nest-user-app/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module.js';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { AllExceptionsFilter } from './middleware/exceptionFilter.js';
+import { LoggerMiddleware } from './middleware/logger.midleware.js';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -10,6 +12,8 @@ async function bootstrap() {
 	app.setGlobalPrefix('api');
 	app.use(helmet());
 	app.use(cookieParser());
+	app.useGlobalFilters(new AllExceptionsFilter());
+	app.use(new LoggerMiddleware().use);
 
 	await app.listen(3000);
 }
--- nest-user-app/src/middleware/logger.midleware.ts
+++ nest-user-app/src/middleware/logger.midleware.ts
@@ -7,25 +7,37 @@ export class LoggerMiddleware implements NestMiddleware {
 	use(req: Request, res: Response, next: NextFunction) {
 		const now = new Date();
 		const timestamp = now.toLocaleString();
+		const { method, originalUrl, hostname, headers } = req;
+
+		console.log(
+			`
+${chalk.green.bold('[LOG]')} ${chalk.white.bgBlack(timestamp)} ${chalk.bold.yellowBright('New Request')}
+${chalk.green('Method:')} ${chalk.blue.bold(method)}
+${chalk.green('URL:')} ${chalk.blue.bold(originalUrl)}
+${chalk.green('Host:')} ${chalk.blue.bold(hostname)}
+${chalk.green('User-Agent:')} ${chalk.blue.bold(headers['user-agent'] || '')}
+      `,
+		);
 
 		const start = Date.now();
-		const { method, originalUrl, hostname, headers } = req;
 
 		res.on('finish', () => {
 			const elapsed = Date.now() - start;
 			const { statusCode } = res;
-			const userAgent = headers['user-agent'] || '';
 
 			console.log(
-				chalk.green.bold(`[LOG] `) +
-					chalk.black(
-						`${chalk.white.bgBlack(timestamp)} ${chalk.bold.yellowBright('New Request')} -`,
-					) +
-					chalk.green(
-						` Method: ${chalk.bold.blue(method)}, URL: ${chalk.bold.blue(originalUrl)}, Host: ${chalk.bold.blue(hostname)}, Status: ${chalk.bold.blue(statusCode)}, Elapsed: ${chalk.bold.blue(elapsed + 'ms')}, User-Agent: ${chalk.bold.blue(userAgent)}`,
-					),
+				`
+${chalk.green.bold('[LOG]')} ${chalk.white.bgBlack(timestamp)} ${chalk.bold.yellowBright('Request Completed')}
+${chalk.green('Method:')} ${chalk.blue.bold(method)}
+${chalk.green('URL:')} ${chalk.blue.bold(originalUrl)}
+${chalk.green('Host:')} ${chalk.blue.bold(hostname)}
+${chalk.green('Status:')} ${chalk.blue.bold(statusCode)}
+${chalk.green('Elapsed:')} ${chalk.blue.bold(`${elapsed}ms`)}
+${chalk.green('User-Agent:')} ${chalk.blue.bold(headers['user-agent'] || '')}
+        `,
 			);
 		});
+
 		next();
 	}
 }
